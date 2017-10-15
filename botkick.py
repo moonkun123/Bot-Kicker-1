18760ad37a07dfc0a8c2bd578c65044cf80acb3e
moonkun123 <moonkun10@gmail.com>
2017-10-15 10:40:47 +0700

    Update botkick.py

--- botkick.py
+++ botkick.py
@@ -9,13 +9,92 @@ import time, datetime, random ,sys, re, string, os, json
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-client = LineClient()
-client._qrLogin("line://au/q/")
+cl = LineClient()
+cl._qrLogin("line://au/q/")
+cl.loginResult()
 
-profile, setting, tracer = client.getProfile(), client.getSettings(), LineTracer(client)
+ki = LineClient()
+ki._qrlogin("line://au/q/")
+ki.loginResult()
+
+profile, setting, tracer = cl.getProfile(), cl.getSettings(), LineTracer(cl)
 offbot, messageReq, wordsArray, waitingAnswer = [], {}, {}, {}
 
-print client._loginresult()
+print "login success"
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+helpmessage =""" [AOZ] Menu Bot Kicker V.1
+Ⓜ[Me]       Cek Akun Sendiri
+Ⓜ[My mid]   Cek Akun Mid
+Ⓜ[Bot?]     Cek Akun Bot
+Ⓜ[Id Group] Cek Id Group
+Ⓜ[Ginfo]    Group Info
+Ⓜ[Mid RA]   Cek All mid Bot
+Ⓜ[RA 1/2/3/4] Cek Mid Bot
+Ⓜ[Respon]   Cek Respon Bot
+Ⓜ[Speedbot] Cek Kecepatan Bot
+Ⓜ[Up]       Fungsi Spam Chat
+Ⓜ[Tagall]   Mention Semua User
+Ⓜ[Gn namagroup] Ganti Nama Group
+Ⓜ[Cancel]  Cancel User Masuk Group
+Ⓜ[Open Url]  Membuka Url Group
+Ⓜ[Close Url]  Menutup Url Group 
+
+􀔃􀅕red arrow right􏿿 Command Private
+Ⓜ[Set group] Menggatur Privasi Grup
+Ⓜ[Banned @] Bann Target 
+Ⓜ[Unban @]  Unbann Target
+Ⓜ[Kill @] Kick Target Bann
+Ⓜ[Nk @]   Kick Target User
+Ⓜ[Invite mid] Invite Via Mid
+Ⓜ[Kick mid] Kick Via mid
+Ⓜ[Ayo masuk] Invite Semua Bot
+Ⓜ[_namabot join] Invite Bot
+Ⓜ[Bye_namabot]  Leave Bot
+"""
+
+Setgroup =""" Privasi Menu V.1 􀔃􀄆red check mark􏿿
+Ⓜ[Protect Group]
+-- Gr on/off
+Ⓜ[Mid Via Contact]
+-- Contact on/off
+Ⓜ[Cancel All Invited]
+-- Cancl on/off
+Ⓜ[No Joinned]
+-- Joinn on/off
+"""
+KAC=[cl,ki,kk,kc]
+DEF=[kk,kc]
+mid = cl.getProfile().mid
+Amid = ki.getProfile().mid
+Bmid = kk.getProfile().mid
+
+Bots=[mid,Amid,Bmid]
+admin=["uc03f8b4d3a35df715cbeeb06f2a33852"]
+wait = {
+    'contact':False,
+    'autoJoin':True,
+    'autoCancel':{"on":True,"members":1},
+    'leaveRoom':True,
+    'timeline':False,
+    'autoAdd':False,
+    'message':"Thanks for add me",
+    "lang":"JP",
+    "comment":"Thanks for add me",
+    "commentOn":False,
+    "commentBlack":{},
+    "wblack":False,
+    "dblack":False,
+    "clock":False,
+    "cName":"ⒶⓄⓏV1",
+    "cName2":"ⒶⓄⓏV2",
+    "blacklist":{},
+    "wblacklist":False,
+    "dblacklist":False,
+    "protectionOn":True,
+    "atjointicket":True
+    }
 
 wait = {
     'readPoint':{},
@@ -48,6 +127,198 @@ def NOTIFIED_ADD_CONTACT(op):
 
 tracer.addOpInterrupt(5,NOTIFIED_ADD_CONTACT)
 
+def bot(op):
+    try:
+        if op.type == 0:
+            return
+        if op.type == 5:
+            if wait["autoAdd"] == True:
+                cl.findAndAddContactsByMid(op.param1)
+                if (wait["message"] in [""," ","\n",None]):
+                    pass
+                else:
+                    cl.sendText(op.param1,str(wait["message"]))
+		
+		 #------Protect Group Kick start------#
+        if op.type == 11:
+           if wait["Protectgr"] == True:
+               if op.param2 not in Bots:
+                   G = ka.getGroup(op.param1)
+                   G.preventJoinByTicket = True
+                   random.choice(DEF).kickoutFromGroup(op.param1,[op.param2])
+                   random.choice(DEF).updateGroup(G)
+        #------Protect Group Kick finish-----#
+
+        #------Cancel Invite User start------#
+        if op.type == 13:
+           if wait["Protectcancl"] == True:
+               if op.param2 not in Bots:
+                  group = ka.getGroup(op.param1)
+                  gMembMids = [contact.mid for contact in group.invitee]
+                  random.choice(DEF).cancelGroupInvitation(op.param1, gMembMids)
+        #------Cancel Invite User Finish------#
+
+	if op.type == 13:
+            if op.param3 in mid:
+                if op.param2 in Amid:
+                    G = Amid.getGroup(op.param1)
+                    G.preventJoinByTicket = False
+                    Amid.updateGroup(G)
+                    Ticket = Amid.reissueGroupTicket(op.param1)
+                    cl.acceptGroupInvitationByTicket(op.param1,Ticket)
+                    G.preventJoinByTicket = True
+                    Amid.updateGroup(G)
+                    Ticket = Amid.reissueGroupTicket(op.param1)
+
+            if op.param3 in Amid:
+                if op.param2 in mid:
+                    X = cl.getGroup(op.param1)
+                    X.preventJoinByTicket = False
+                    cl.updateGroup(X)
+                    Ti = cl.reissueGroupTicket(op.param1)
+                    ki.acceptGroupInvitationByTicket(op.param1,Ti)
+                    X.preventJoinByTicket = True
+                    ki.updateGroup(X)
+                    Ti = ki.reissueGroupTicket(op.param1)
+
+            if op.param3 in Bmid:
+                if op.param2 in Amid:
+                    X = ki.getGroup(op.param1)
+                    X.preventJoinByTicket = False
+                    ki.updateGroup(X)
+                    Ti = ki.reissueGroupTicket(op.param1)
+                    kk.acceptGroupInvitationByTicket(op.param1,Ti)
+                    X.preventJoinByTicket = True
+                    kk.updateGroup(X)
+                    Ti = kk.reissueGroupTicket(op.param1)
+			
+			 if op.type == 13:
+            if mid in op.param3:
+                G = cl.getGroup(op.param1)
+                if wait["autoJoin"] == True:
+                    if wait["autoCancel"]["on"] == True:
+                        if len(G.members) <= wait["autoCancel"]["members"]:
+                            cl.rejectGroupInvitation(op.param1)
+                        else:
+                            cl.acceptGroupInvitation(op.param1)
+                    else:
+                        cl.acceptGroupInvitation(op.param1)
+                elif wait["autoCancel"]["on"] == True:
+                    if len(G.members) <= wait["autoCancel"]["members"]:
+                        cl.rejectGroupInvitation(op.param1)
+            else:
+                Inviter = op.param3.replace("",',')
+                InviterX = Inviter.split(",")
+                matched_list = []
+                for tag in wait["blacklist"]:
+                    matched_list+=filter(lambda str: str == tag, InviterX)
+                if matched_list == []:
+                    pass
+                else:
+                    cl.cancelGroupInvitation(op.param1, matched_list)
+                    
+        if op.type == 22:
+            if wait["leaveRoom"] == True:
+                cl.leaveRoom(op.param1)
+        if op.type == 24:
+            if wait["leaveRoom"] == True:
+                cl.leaveRoom(op.param1)
+        if op.type == 26:
+            msg = op.message
+
+
+            if msg.toType == 1:
+                if wait["leaveRoom"] == True:
+                    cl.leaveRoom(msg.to)
+            if msg.contentType == 16:
+                url = msg.contentMetadata("line://home/post?userMid="+mid+"&postId="+"new_post")
+                cl.like(url[25:58], url[66:], likeType=1001)
+        if op.type == 26:
+            msg = op.message
+            if msg.contentType == 13:
+               if wait["wblack"] == True:
+                    if msg.contentMetadata["mid"] in wait["commentBlack"]:
+                        cl.sendText(msg.to,"already")
+                        wait["wblack"] = False
+                    else:
+                        wait["commentBlack"][msg.contentMetadata["mid"]] = True
+                        wait["wblack"] = False
+                        cl.sendText(msg.to,"decided not to comment")
+
+               elif wait["dblack"] == True:
+                   if msg.contentMetadata["mid"] in wait["commentBlack"]:
+                        del wait["commentBlack"][msg.contentMetadata["mid"]]
+                        cl.sendText(msg.to,"deleted")
+                        ki.sendText(msg.to,"deleted")
+                        kk.sendText(msg.to,"deleted")
+                        kc.sendText(msg.to,"deleted")
+                        wait["dblack"] = False
+
+                   else:
+                        wait["dblack"] = False
+                        cl.sendText(msg.to,"It is not in the black list")
+                        ki.sendText(msg.to,"It is not in the black list")
+                        kk.sendText(msg.to,"It is not in the black list")
+                        kc.sendText(msg.to,"It is not in the black list")
+               elif wait["wblacklist"] == True:
+                   if msg.contentMetadata["mid"] in wait["blacklist"]:
+                        cl.sendText(msg.to,"already")
+                        ki.sendText(msg.to,"already")
+                        kk.sendText(msg.to,"already")
+                        kc.sendText(msg.to,"already")
+                        wait["wblacklist"] = False
+                   else:
+                        wait["blacklist"][msg.contentMetadata["mid"]] = True
+                        wait["wblacklist"] = False
+                        cl.sendText(msg.to,"aded")
+                        ki.sendText(msg.to,"aded")
+                        kk.sendText(msg.to,"aded")
+                        kc.sendText(msg.to,"aded")
+
+               elif wait["dblacklist"] == True:
+                   if msg.contentMetadata["mid"] in wait["blacklist"]:
+                        del wait["blacklist"][msg.contentMetadata["mid"]]
+                        cl.sendText(msg.to,"deleted")
+                        ki.sendText(msg.to,"deleted")
+                        kk.sendText(msg.to,"deleted")
+                        kc.sendText(msg.to,"deleted")
+                        wait["dblacklist"] = False
+
+                   else:
+                        wait["dblacklist"] = False
+                        cl.sendText(msg.to,"It is not in the black list")
+                        ki.sendText(msg.to,"It is not in the black list")
+                        kk.sendText(msg.to,"It is not in the black list")
+                        kc.sendText(msg.to,"It is not in the black list")
+               elif wait["contact"] == True:
+                    msg.contentType = 0
+                    cl.sendText(msg.to,msg.contentMetadata["mid"])
+                    if 'displayName' in msg.contentMetadata:
+                        contact = cl.getContact(msg.contentMetadata["mid"])
+                        try:
+                            cu = cl.channel.getCover(msg.contentMetadata["mid"])
+                        except:
+                            cu = ""
+                        cl.sendText(msg.to,"[displayName]:\n" + msg.contentMetadata["displayName"] + "\n[mid]:\n" + msg.contentMetadata["mid"] + "\n[statusMessage]:\n" + contact.statusMessage + "\n[pictureStatus]:\nhttp://dl.profile.line-cdn.net/" + contact.pictureStatus + "\n[coverURL]:\n" + str(cu))
+                    else:
+                        contact = cl.getContact(msg.contentMetadata["mid"])
+                        try:
+                            cu = cl.channel.getCover(msg.contentMetadata["mid"])
+                        except:
+                            cu = ""
+                        cl.sendText(msg.to,"[displayName]:\n" + contact.displayName + "\n[mid]:\n" + msg.contentMetadata["mid"] + "\n[statusMessage]:\n" + contact.statusMessage + "\n[pictureStatus]:\nhttp://dl.profile.line-cdn.net/" + contact.pictureStatus + "\n[coverURL]:\n" + str(cu))
+            elif msg.contentType == 16:
+
+def Chivas(op):
+    try:
+        sendMessage(op.param1, client.getContact(op.param1).displayName + "Iya Ada Apa?")
+    except Exception as e:
+        print e
+        print ("\n\nChivas\n\n")
+        return
+
+tracer.addOpInterrupt(5,Chivas)
+
 def NOTIFIED_ACCEPT_GROUP_INVITATION(op):
     #print op
     try:
@@ -78,6 +349,53 @@ def NOTIFIED_LEAVE_GROUP(op):
         return
 
 tracer.addOpInterrupt(15,NOTIFIED_LEAVE_GROUP)
+
+  #-------------Fungsi Spam Start---------------------#
+            elif msg.text in ["Up","up","Up Chat","Up chat","up chat","Upchat","upchat"]:
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                cl.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                ki.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+                kk.sendText(msg.to,"P 􀔃􀆶squared up!􏿿")
+        #-------------Fungsi Spam Finish---------------------#
+
+#-------------Fungsi Broadcast Start------------#
+            elif "Bc " in msg.text:
+				bctxt = msg.text.replace("Bc ","")
+				cl.sendText(msg.to,(bctxt))
+				ki.sendText(msg.to,(bctxt))
+				kk.sendText(msg.to,(bctxt))
+       #--------------Fungsi Broadcast Finish-----------#
+
+            elif msg.text in ["Cv Absen"]:
+	        cl.sendText(msg.to,"Hadiirrr 􀜁􀅔Har Har􏿿")
+                ki.sendText(msg.to,"Hadiirrr 􀜁􀅔Har Har􏿿")
+                kk.sendText(msg.to,"Hadiirrr 􀜁􀅔Har Har􏿿")
 
 def NOTIFIED_READ_MESSAGE(op):
     #print op
@@ -154,7 +472,7 @@ def SEND_MESSAGE(op):
                     sendMessage(msg.to, "%sseconds" % (elapsed_time))
 				
 		if msg.text == "Mun":
-                    sendMessage(msg.to,"Iya ada apa?".displayname)
+                    sendMessage(msg.to,"Iya ada apa?")
 #-------------------------------------------------------------		
         else:
             pass
